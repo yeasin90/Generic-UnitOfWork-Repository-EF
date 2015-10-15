5422e30f40c55ecac54c6bdd0252043aa2a325b3
six_sic6 <abedinyeasin@gmail.com>
2015-10-15 09:07:46 +0600

    added consumer engine and separate Data project

---
+++ Consumer/Program.cs
@@ -0,0 +1,24 @@
+﻿using Data;
+using Data.Uof;
+using DBFirstEF.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UnitOfWork uof = new UnitOfWork(ContextType.AdventureWorks2012Context);
+
+            uof.GetRepo<ContactTypeRepository>().Insert(new ContactType { ContactTypeID = 100, ModifiedDate = DateTime.Now, Name = "Type1" });
+            uof.GetRepo<ContactTypeRepository>().Insert(new ContactType { ContactTypeID = 101, ModifiedDate = DateTime.Now, Name = "Type2" });
+            uof.GetRepo<AddressTypeRepository>().Insert(new AddressType { AddressTypeID = 100, ModifiedDate = DateTime.Now, Name = "Type100", rowguid = new Guid() });
+            uof.Commit();
+        }
+    }
+}
---
+++ Data/Uof/ContextFactory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Uof
+{
+    public static class ContextFactory
+    {
+        public static DbContext GetContext(ContextType type)
+        {
+            switch (type)
+            {
+                case ContextType.AdventureWorks2012Context:
+                    return new AdventureWorks2012Entities();
+                default:
+                    return null;
+            }
+        }
+    }
+}
